316120a462323071b62df1728a1d274259e4642b
Adrian Tesore <adrian72112@gmail.com>
2018-09-07 21:46:40 -0300

    Fuentes del programa

---
+++ src/Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Library;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library.TemperatureReporter reporteador = new Library.TemperatureReporter();
+            Library.TemperatureMonitor monitor = new Library.TemperatureMonitor();
+            reporteador.Subscribe(monitor);
+            monitor.GetTemperature();
+        }
+    }
+}
